574a36310e402e7aa79cd401259967db483c0886
Emmanuel <emmanuel.feygelson@gmail.com>
2018-01-31 11:29:14 -0800

    Ready to style

--- frontend/ezStay.jsx
+++ frontend/ezStay.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { signup, login, logout } from './util/session_api_util';
 import configureStore from './store/store';
 import Root from './components/root';
 import * as ActionsUtil from './actions/session_actions';
 
-// window.login = login;
-// window.signup = signup;
-// window.logout = logout;
 window.actions = ActionsUtil;
 
 document.addEventListener('DOMContentLoaded', () => {
+  let store;
 
-  const store = configureStore();
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
+  if (window.currentUser) {
+    const preloadedState = {session: { currentUser: window.currentUser }};
+    store = configureStore(preloadedState);
+    delete window.currentUser;
+  } else { store = configureStore(); }
+
   const root = document.getElementById('root');
-  ReactDOM.render(<Root store={ store} />, root);
+  ReactDOM.render(<Root store={store} />, root);
+
 });
