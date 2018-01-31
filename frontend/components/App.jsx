574032d4eb3845b67e0ef740b5b532bee13157cc
Emmanuel <emmanuel.feygelson@gmail.com>
2018-01-31 10:51:15 -0800

    protected routes

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -2,14 +2,16 @@ import React, { Fragment } from 'react';
 import GreetingContainer from './greeting/greeting_container';
 import SessionFormContainer from './session_form/session_form_container';
 import { Route } from 'react-router-dom';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 const App = () => (
   <Fragment>
     <h1>ezStay</h1>
     <GreetingContainer />
 
-    <Route path="/login" component={SessionFormContainer} />
-    <Route path="/signup" component={SessionFormContainer} />
+    <AuthRoute exact path="/login" component={SessionFormContainer} />
+    <AuthRoute exact path="/signup" component={SessionFormContainer} />
+
   </Fragment>
 );
 
