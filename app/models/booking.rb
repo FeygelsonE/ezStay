fdd92b54fad644db14729cf8671461580c0c6e3d
Emmanuel <emmanuel.feygelson@gmail.com>
2018-02-09 10:32:33 -0800

    Add comma to booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,7 +1,7 @@
 class Booking < ApplicationRecord
   validates :user_id, :location_id, :start_date, :end_date, presence: true
 
-  belongs_to :location
+  belongs_to :location,
     primary_key: :id,
     foreign_key: :location_id,
     class_name: 'Location'
